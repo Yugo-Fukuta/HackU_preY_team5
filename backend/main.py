4d5c5e83b51c56735739ecf377640931f4d81c9b
ThunderRa1n <Podsn0w@gmail.com>
2021-06-21 20:38:55 +0900

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,9 @@
-'''from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def index():
-    return {"Hello": "World"}'''
-
 from fastapi import FastAPI, APIRouter
 from apis.oshido import router as oshido_router
 
 router = APIRouter()
 router.include_router(
     oshido_router,
-    #prefix='/users',
-    #tags=['users']
 )
 
 app = FastAPI()
