a7959654f644c65868a2bc163e0fceddc8291cb1
w-shimaya <w.shimaya@gmail.com>
2021-06-27 15:10:08 +0900

    [fix] 実行時間短縮

--- backend/apis/api_components/wikipedia_fetch.py
+++ backend/apis/api_components/wikipedia_fetch.py
@@ -1,6 +1,11 @@
 import argparse
+import io
+import json
 import lxml.etree
+import pycurl
 import wptools
+import time
+import urllib.parse
 
 
 def wikipedia_fetch_prof(q, silent=True):
@@ -9,17 +14,24 @@ def wikipedia_fetch_prof(q, silent=True):
 
     # 日本人でも日本語版のinfoboxが（なぜか）取れない人がいるので
     for l in langs:
-        try:
-            so = wptools.page(q, lang=l, boxterm="Infobox", silent=silent).get_parse()
-        except LookupError as e:
-            continue
+        # https://www.mediawiki.org/wiki/API:Parsing_wikitext/ja
+        # 大いにwptoolsのソースを引用
+        url = (f"https://{l}.wikipedia.org/w/api.php?"
+               f"action=parse&page={urllib.parse.quote(q)}"
+               "&prop=parsetree&format=json&redirect")
+        with io.BytesIO() as b:
+            curl = pycurl.Curl()
+            curl.setopt(pycurl.URL, url)
+            curl.setopt(pycurl.WRITEFUNCTION, b.write)
+            curl.perform()
+            body = b.getvalue()
+        data = json.loads(body)
+        ptree = data["parse"]["parsetree"]["*"]
 
-        infobox = so.data["infobox"]
-        if infobox:
-            return infobox
-
-        # infobox が取れなかったらそれっぽいものを探す
-        for item in lxml.etree.fromstring(so.data["parsetree"]).xpath(f"//template/part/value[translate(normalize-space(text()),' ','')='{q}']/../.."):
+        # infobox らしいものを探す
+        for item in lxml.etree.fromstring(ptree).\
+                xpath("//template/part/value[translate("
+                        f"normalize-space(text()),' ','')='{q}']/../.."):
             box = wptools.utils.template_to_dict(item)
             if box:
                 return box
@@ -35,6 +47,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
-        print(wikipedia_fetch_prof(args.q, silent=args.silent))
+        start = time.time()
+        res = wikipedia_fetch_prof(args.q, silent=args.silent)
+        end = time.time()
+        print(res, "etime: ", end - start)
     except Exception as e:
         print(e)
